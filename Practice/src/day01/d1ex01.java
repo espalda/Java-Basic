44a938f15307a463f61ecb31d19a6b8a1aeabc07
espalda <espalda@naver.com>
2019-04-04 17:45:45 +0900

    d1 자료형 변수명

--- Practice/src/day01/d1ex01.java
+++ Practice/src/day01/d1ex01.java
@@ -3,21 +3,24 @@ package day01;
 public class d1ex01 {
 
 	public static void main(String[] args) {
-		/* 자료형 변수명
+		/* d1 자료형 변수명
 		 * println("문자열") 내가 원하는 문자열을 콘솔에 출력한 후 엔터를 입력 sysout + ctrl + space
 		 * print("문자열" 내가 원하는 문자열을 콘솔에 출력
-		 * 
+
 		 * 논리형 boolean
 		 * 문자형 char
 		 * 정수형 byte, short, int, long
 		 * 실수형 float, double
-		 * 
+
 		 * byte = 1byte
 		 * short, ch = 2byte
 		 * int = 4byte
 		 * long = 8byte
 		 * float = 4byte 유효숫자는 7자리까지
 		 * double = 8byte 유효숫자는 15자리까지
+		 
+		 * 변수 : 값을 저장할수 있는 메모리상의 공간, 단 하나의 값만 저장
+		 * 변수의 초기화 : 변수를 사용하기 전에 처음으로 값을 저장하는 것
 		 */
 	
 		int num = 0;
@@ -36,9 +39,6 @@ public class d1ex01 {
 		int i = 20;
 		boolean isEven = i % 2 == 0 ? true:false;
 		System.out.println(i + "은 짝수? " + isEven);
-		
-		//프로그램에서의 변수란? 값을 저장할수 있는 메모리상의 공간, 단 하나의 값만 저장
-		//변수의 초기화안? 변수를 사용하기 전에 처음으로 값을 저장하는 것
 		
 	}
 
