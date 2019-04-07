91e8d5545c5b43a26134932ad756742b611650db
espalda <espalda@naver.com>
2019-04-08 00:28:32 +0900

    push

--- Practice/src/day09/d9class01.java
+++ Practice/src/day09/d9class01.java
@@ -3,10 +3,22 @@ package day09;
 import java.util.Scanner; 
 // java.utill이라는 package에  scanner 라는 class를 가져오겠다
 // 다른 패키지 안에 있는 클래스를 사용하려면 import package.class;을 선언해야 해당 클래스를 사용가능
-public class d9ex01 {
+public class d9class01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* 클래스멤버와 객체멤버
+		 * - 객체멤버
+		 * 객체가 생성된 후에 사용할 수 있다 =new Tv
+		 * 객체.멤버변수, 객체.멤버메서드로 호출
+		 * 객체멤버는 만들어진 객체마다 가지고 있다
+
+		 * - 클래스멤버
+		 * 객체의 생성 없이 사용할수 있다
+		 * 클래스.멤버변수, 클래스,멤버메서드로 호출
+		 * 클래스멤버는 하나씩만 만들어진다
+		 * Static 붙고 공유된다
+		 */
+		
 		Scanner scan = new Scanner(System.in); //scan 은 객체
 		int num = scan.nextInt(); 
 		//nextInt()는 Scanner 클래스의 멤버메서드
--- Practice/src/day09/d9class02.java
+++ Practice/src/day09/d9class02.java
@@ -2,12 +2,12 @@ package day09;
 
 import day08.d8ex04;
 
-public class d9ex02 {
+public class d9class02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String []s = new String[3];
-		LgtvTest2.main(s);
+		Televisiontest.main(s);
 		
 		int res = d8ex04.factorial(5);
 		System.out.println(res);
