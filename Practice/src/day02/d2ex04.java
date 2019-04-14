f54edb5815d1179b6d7fcfd31c45981180f3dd97
espalda <espalda@naver.com>
2019-04-14 22:46:21 +0900

    d2 중첩 if문

--- Practice/src/day02/d2ex04.java
+++ Practice/src/day02/d2ex04.java
@@ -3,14 +3,15 @@ package day02;
 public class d2ex04 {
 
 	public static void main(String[] args) {
-		// d2 중첩 if문
-		// 일반 if문
+		/* d2 중첩 if문
+		 * 일반 if문
+		 */
 		int num = 12;
 		if(num % 2 == 0 && num % 3 == 0){
 			System.out.println(num + "은 6의배수");
 		}
 		
-		// 중첩 if문
+		//중첩 if문
 		int num1 = 12;
 		if(num1 % 2 == 0){
 			if(num1 % 3 == 0){
