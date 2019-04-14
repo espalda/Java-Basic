8578bbbcf23e49f16b620828a1593a503d2564cb
espalda <espalda@naver.com>
2019-04-14 23:15:36 +0900

    d11 equal

--- Practice/src/day11/d11equal.java
+++ Practice/src/day11/d11equal.java
@@ -3,6 +3,7 @@ package day11;
 public class d11equal {
 
 	public static void main(String[] args) {
+		//d11 equal
 		String str1 = "Hello", str2 = "Hello";
 		boolean cmp = str1 == str2; //같다를 통해서 문자열 비교 가능
 		System.out.println(cmp);
