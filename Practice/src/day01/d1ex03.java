d2d103c8299a625e9bdff4d9425bacf614c0555b
espalda <espalda@naver.com>
2019-04-14 19:58:54 +0900

    d1 형변환

--- Practice/src/day01/d1ex03.java
+++ Practice/src/day01/d1ex03.java
@@ -8,23 +8,20 @@ public class d1ex03 {
 		 * 명시적 형변환 : 데이터 손실이 발생할수도 있다
 		 */
 		
-		//문제 10을 3으로 나눈 결과 출력 문제
-		//방법01
+		//10을 3으로 나눈 결과 출력
  		int num = 10;
  		int num0 = 3;
  		System.out.println((double) num / num0);
 
-		//방법02
 		System.out.println((double) 10 / 3);
 		
-		//방법03
 		double res;
 		res = num / (double) num0;
 		System.out.println(res);
 		
 		/* 문자 + 정수 = 정수
 		 * 실수 + 정수 = 실수
-		 * 정수/정수 = 정수
+		 * 정수 / 정수 = 정수
 		 * 실수로 나누면 전부 결과값이 전부 실수로 출력
 		 */
 		int num1 = 1 + 2;
