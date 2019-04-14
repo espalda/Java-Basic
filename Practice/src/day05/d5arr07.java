20f21fad786155cf2d5b4d10e457abb2095dbfd9
espalda <espalda@naver.com>
2019-04-14 22:53:12 +0900

    d5 배열 공약수 배열

--- Practice/src/day05/d5arr07.java
+++ Practice/src/day05/d5arr07.java
@@ -3,19 +3,19 @@ package day05;
 public class d5arr07 {
 
 	public static void main(String[] args) {
-		/* d5 공약수 배열저장
+		/* d5d5 배열 공약수 배열
 		 * 두수의 공약수들을 배열에 저장하여 출력하는 코드 작성
 		 * 최대 10개를 저장 가능
 		 */
 		
 		int [] arr = new int [10];
 		int i;
-		int cnt=0; // 공약수의 갯수
+		int cnt=0; //공약수의 갯수
 		int num1 = 8;
 		int num2 = 12;
 		for(i=1 ; i<=num1 ; i++){
 			if(cnt == 10){
-				break; // 10개 이상은 더이상 구하지 말라
+				break; //10개 이상은 더이상 구하지 말라
 			}
 			if(num1 % i == 0 && num2 % i == 0){
 				arr[cnt] = i;
