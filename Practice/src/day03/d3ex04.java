6708cd0463868aa04fcd696f136532d46a248519
espalda <espalda@naver.com>
2019-04-14 22:50:50 +0900

    d3 반복문 소수

--- Practice/src/day03/d3ex04.java
+++ Practice/src/day03/d3ex04.java
@@ -1,36 +1,48 @@
 package day03;
 
+import java.util.Scanner;
+
 public class d3ex04 {
 
 	public static void main(String[] args) {
-		//d3 반복문 구구단
-		//문제01 구구단 7단을 출력하는 반복 코드
-		int i, sum;
-			 for (i=1, sum=0 ; i<=9 ; i++){
-				 sum = 7 * i;
-				 System.out.println("7 X " + i + " = " + sum);
+		 /* d3 반복문 소수
+		  * 소수 : 약수가 1과 자기자신만 있는 수 약수가 2개인 수
+		  * 약수 : 자신보다 작거나 같은 수 중 나누어 나머지가 0인수 
+		  * num : 정수
+		  * i : 반복문에서 사용할 변수
+		  * cnt : 약수의 갯수
+		  * 반복횟수 : i는 1부터 num보다 작거나 같을 때까지 1씩 증가
+		  */
+		
+		//정수를 입력받아 입력받은 정수가 소수인지 아닌지 판별하는 반복문 코드
+		int num, i, cnt;
+		Scanner scan = new Scanner(System.in);
+		num = scan.nextInt();
+		for (i=1, cnt=0; i<=num; i++){
+			
+			//i가 num의 약수이면 약수의 갯수를 하나 증가
+			//num을 i로 나누었을 때 나머지가 0과 같다 = i가 num의 약수
+			
+			if(num % i == 0){
+				cnt++; //cnt +=1; ++cnt; cnt=cnt+1;
+				}
 		}
-		
-		//문제02 구구단 7단을 출력하는 반복 코드
-		int num = 3;
-		int res = 0;
-			for (i=1 ; i<=9 ; i++){
-			res = num * i;
-			System.out.println(num + " x " + i + " = " + res);
+		//반복문 종료 후 약수의 갯수가 2개이면 소수라고 출력
+		if(cnt == 2){
+				System.out.println(num+ "는 소수");
 		}
-		
-		//문제03 구구단 전체 나오는 방법	
-		for(num=2; num<=9; num++){
-			System.out.println(num + " 단");
-			for (i=1 ; i<=9 ; i++){
-				res = num * i;
-				System.out.println(num + " x " + i + " = " + res);
-			}
+		//아니면 소수가 아님으로 출력 
+		else{
+			System.out.println(num+ "은 소수가 아님");
 		}
-		
-		
-}
-}		
 	
+		scan.close();
+	
+		
+		
+				
+		
+		
+	}
 
-
+}
