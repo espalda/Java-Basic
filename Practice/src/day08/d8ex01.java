ad0ad11f6ab40d0551629ebb3a0b784259e66dad
espalda <espalda@naver.com>
2019-04-14 23:03:27 +0900

    d8 메서드 복습

--- Practice/src/day08/d8ex01.java
+++ Practice/src/day08/d8ex01.java
@@ -3,13 +3,13 @@ package day08;
 public class d8ex01 {
 
 	public static void main(String[] args) {
-		// d8 메서드 복습
+		//d8 메서드 복습
 		sum(1,3);
-		// 이러한 형태는 의미가 없다
+		//이러한 형태는 의미가 없다
 		
 		int res = sum(1,2);
 		System.out.println(res);
-		// int res를 대입해서 사용할수 있다
+		//int res를 대입해서 사용할수 있다
 	}
 		/* 기능 : 두 정수의 합을 알려주는 메서드
 		 * 매개변수 : int num1, int num2
