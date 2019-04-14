9246810709d57245c2d1c711b5913b193ee8ef80
espalda <espalda@naver.com>
2019-04-14 22:46:37 +0900

    d2 반복문 for

--- Practice/src/day02/d2for01.java
+++ Practice/src/day02/d2for01.java
@@ -1,20 +1,20 @@
 package day02;
 
-public class d2ex08 {
+public class d2for01 {
 
 	public static void main(String[] args) {
 		/* d2 반복문 for
 		 * 반복문 for : 규칙성을 찾아야 함
-		 * for(초기화 ; 조건식 ; 증감연산) {
-		 * 		실행문; } 실행문이 한줄이면 중괄호 생략 가능
+		 * for(초기화 ; 조건식 ; 증감식) {
+		 * 		실행문;} 실행문이 한줄이면 중괄호 생략 가능
 		 * i는 반복문에서 사용할 변수
 		 */
 		
 		int i = 0; 
-		for(i=1; i<=5; i++){
+		for(i=1; i<=5; i++){	//i++대신 i=i+1 또는 i+=1  사용 가능
 			System.out.println("Hello World!");
 		}
-		// i++대신 i=i+1 또는 i+=1  사용 가능
+		
 	}
 
 }
