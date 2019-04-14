8f226d9f7a141961a3f4c80fc20f482c03aa86d6
espalda <espalda@naver.com>
2019-04-14 19:59:01 +0900

    d1 스캐너 입력

--- Practice/src/day01/d1ex04.java
+++ Practice/src/day01/d1ex04.java
@@ -11,7 +11,7 @@ public class d1ex04 {
 		 * scan.close();
 		 */
 		
-		//문제01 콘솔에서 입력받은 숫자가 1보다 크거나 같고 100보다 작거나 같은지 출력
+		//콘솔에서 입력받은 숫자가 1보다 크거나 같고 100보다 작거나 같은지 출력
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
 		
@@ -23,7 +23,7 @@ public class d1ex04 {
 		isScore = num >= 1 && num <= 100;
 		System.out.println(isScore);
 
-		//문제02 콘솔에서 입력받은 숫자의 산술연산 결과 출력
+		//콘솔에서 입력받은 숫자의 산술연산 결과 출력
 		int num1 = scan.nextInt();
 		int num2 = scan.nextInt();
 			System.out.println(num1 + num2);
