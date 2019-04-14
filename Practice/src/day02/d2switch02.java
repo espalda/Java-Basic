6d3921300416f4da16f4647df3692a1977bf2fbc
espalda <espalda@naver.com>
2019-04-14 22:49:21 +0900

    d2 switch 홀수짝수

--- Practice/src/day02/d2switch02.java
+++ Practice/src/day02/d2switch02.java
@@ -1,17 +1,17 @@
 package day02;
 
-public class d2switch06 {
+public class d2switch02 {
 
 	public static void main(String[] args) {
-		// d2 switch 홀짝 구분
-		// 문제 : switch문으로 홀수 짝수 구분
+		//d2 switch 홀수짝수
+		//switch문으로 홀수 짝수 구분
 		int num = 9;
 		switch(num % 2){
-		case 0://=if(num % 2=0}
+		case 0:		//=if(num % 2=0}
 			System.out.println("짝수");;
 			break;
 		//..
-		//case 1://=default:
+		//case 1:	//=default:
 		default:
 			System.out.println("홀수");;
 			break;
