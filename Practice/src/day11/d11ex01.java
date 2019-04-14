152073cdaa0085c4415cc3ee14522da25ae6b5a3
espalda <espalda@naver.com>
2019-04-14 23:15:46 +0900

    d11 익명 클래스 anonymous class

--- Practice/src/day11/d11ex01.java
+++ Practice/src/day11/d11ex01.java
@@ -7,7 +7,6 @@ public class d11ex01 {
 		 * 클래스의 선언과 객체의 생성을 동시에 하기 때문에 한번만 사용될수 있고
 		 * 오직 하나의 객체만을 생성할수 있는 일회용 클래스이다
 		 */
-		
 		print(new A (){
 			public void test(){
 				System.out.println("test");
@@ -21,7 +20,7 @@ public class d11ex01 {
 interface A{
 	void test();
 }
-/*class B implements A{
-	public void test(){}
-}
-*/
+/* class B implements A{
+ * public void test(){}
+ * }
+ */
