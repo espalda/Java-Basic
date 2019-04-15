e91e38757f3428bb29b8dbf4c6629f5a5148eb01
espalda <espalda@naver.com>
2019-04-15 17:06:12 +0900

    d2 반복문 for

--- Practice/src/day02/d2for01.java
+++ Practice/src/day02/d2for01.java
@@ -4,6 +4,7 @@ public class d2for01 {
 
 	public static void main(String[] args) {
 		/* d2 반복문 for
+		 * 규칙성 있게 반복하는 동작을 코드로 작성
 		 * 반복문 for : 규칙성을 찾아야 함
 		 * for(초기화 ; 조건식 ; 증감식) {
 		 * 		실행문;} 실행문이 한줄이면 중괄호 생략 가능
