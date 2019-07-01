48ad167391f24e53c376c37d71c87060720e5c98
espalda <espalda@naver.com>
2019-07-01 21:36:42 +0900

    java review

--- example/src/review/Point.java
+++ example/src/review/Point.java
@@ -1,4 +1,4 @@
-package Test1;
+package review;
 
 public class Point {
 
--- example/src/review/test1.java
+++ example/src/review/test1.java
@@ -1,10 +1,12 @@
-package Test2;
+package review;
 
-public class ex1 {
+public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int i = 10;
+		System.out.println(i);
 	}
 
 }
