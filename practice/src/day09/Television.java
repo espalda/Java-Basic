89b9e9c296a7deccc3670a23152d9464cc3fd608
espalda <espalda@naver.com>
2019-07-01 21:13:58 +0900

    java 9일차 Television 클래스

---
+++ practice/src/day09/Television.java
@@ -0,0 +1,81 @@
+package day09;
+
+public class Television {
+	//d9 클래스 Television
+	
+	private int channel;
+	private int volume;
+	private final int MAX_CHANNEL = 500;
+	private final int MAX_VOLUME = 100;
+	
+	//클래스 멤버변수
+	public static final String BRAND = "LG";
+	
+	public void channelUP(){
+		if(channel < MAX_CHANNEL)
+			channel++;
+	}
+	public void channelDown(){
+		if(channel > 1)
+			channel--;
+	}	
+	public void setChannel(int ch){
+		if(ch == 0)
+			channel = 1;
+		else if(ch > MAX_CHANNEL)
+			channel = MAX_CHANNEL;
+		else
+			channel = ch;
+	}
+	
+	public void volumeUP(){		//하나의 메서드로 만들수 없을까? 볼륨 증가 감소가..
+		if(volume < MAX_VOLUME)	//하나의 메서드에 증가 감소 만들기 +라고 입력하면 증가, -라고 입력하고 감소
+			volume++;
+	}
+	public void volumeDown(){
+		if(volume > 0)
+			volume--;
+	}
+	
+	public void printVolume (){
+		System.out.println("소리크기: "+ volume);
+	}
+	public void printChannel (){
+		System.out.println("채널번호: "+ channel);
+		/* 객체 멤버메소드에는 클래스 멤버변수 또는 클래스 멤버메소드를 사용할수 있다
+		 * System.out.println(BRAND);
+		 */
+	}
+	public static void printBrand(){
+		System.out.println("브랜드: "+ BRAND);
+		/* 클래스 멤버메소드에는 클래스 멤버변수는 사용 가능하지만
+		 * 객체 멤버변수는 사용할 수 없다
+		 * System.out.println(channel);
+		 */
+	}
+		/* 기본 생성자
+		 * 멤버 메소드는 운하는 시점에서 호출이 가능하지만
+		 * 생성자는 객체를 생성할 때에만 호출한다
+		 * 객체멤버변수를 초기화한다
+		 */
+		public Television() {
+			this(8, 10);
+			//channel = 1;
+			//volume = 10;
+		}
+		//기본 생성자가 있어야 생성자 오버로딩으로 설정 가능함
+		public Television(int channel, int volume) {
+			setChannel(channel);
+			if(volume > MAX_VOLUME)
+				this.volume = MAX_VOLUME;
+			else
+				this.volume = volume;
+		}
+		//복사 생성자
+		public Television(Television stv){
+			this(stv.channel, stv.volume);
+			//channel =stv.channel;
+			//volume = stv.volume;
+			
+		}
+}
---
+++ practice/src/day09/TelevisionTest.java
@@ -0,0 +1,40 @@
+package day09;
+
+public class TelevisionTest {
+
+	public static void main(String[] args) {
+		//d9 클래스 Television test
+		//기본 생성자를 이용한 객체 생성 후 초기화
+		Television t1 = new Television();
+		t1.printChannel();
+		t1.printVolume();
+		t1.printBrand();
+		System.out.println(Television.BRAND);
+		
+		
+		//생성자 오버로딩을 이용한 객체 생성 후 초기화
+		Television t2 = new Television(100,15);
+		t2.printChannel();
+		t2.printVolume();
+		
+		
+		//t3를 생성해서 t2의 값으로 초기화
+		Television t3 = new Television(t2);
+		t3.printChannel();
+		t3.printVolume();
+		t3.volumeDown();
+		
+		System.out.println("t3 볼륨감소 실행후 t3");
+		t3.printVolume();
+		System.out.println("t3 볼륨감소 실행후 t2");
+		t2.printVolume();
+		
+		//t4를 생성하지 않고 t3의 다른이름으로 t4라 부른다
+		Television t4 = t3;
+		System.out.println("t3 볼륨감소 실행후 t4");
+		t4.printVolume();
+
+	}
+
+}
+
