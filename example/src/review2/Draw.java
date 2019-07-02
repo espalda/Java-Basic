4d48d9efcaa9457c8a930e67382671b06e6bc9d6
espalda <espalda@naver.com>
2019-07-02 14:46:21 +0900

    java 인터페이스 복습

---
+++ example/src/review2/Draw.java
@@ -0,0 +1,21 @@
+package review2;
+
+interface Draw {
+	public void resize(int width, int height);
+	public void move(int x, int y);
+}
+class Rect implements Draw{
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
